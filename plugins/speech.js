be908995130d2206b3cbcf02ad3bfaa27816f196
Kirill Live <kirill_@live.ru>
2023-08-31 22:06:06 +0300

    kirilllive

--- plugins/speech.js
+++ plugins/speech.js
@@ -3,8 +3,8 @@ tuesday.addEventListener('creation_dialog',()=>{
     if(tue_set_audio<2&&story_json.parameters.text_panel.speech||arr_dialog.speech){
         const speech=new SpeechSynthesisUtterance(dialog_text);
         if (synth.speaking){synth.cancel()}
-        let voice=(arr_dialog.speech)?arr_dialog.speech[languare]:story_json.parameters.text_panel.speech[languare];
-        let all_voice=speechSynthesis.getVoices(),arr_voice=voice[0].split(",").map((item)=>{return item.trim()});
+        let voice=(arr_dialog.speech&&arr_dialog.speech[languare][0].length>0)?arr_dialog.speech[languare]:story_json.parameters.text_panel.speech[languare];
+        let all_voice=speechSynthesis.getVoices(),arr_voice=voice[0].split(',').map((item)=>{return item.trim()});
         let voice_name=false;
         for(v=0;v<all_voice.length;v++){
             all_voice.filter((voice)=>{if(voice.name==arr_voice[v]){voice_name=voice}})
--- plugins/tue_aspect_ratio.js
+++ plugins/tue_aspect_ratio.js
@@ -4,20 +4,20 @@ function TueAspectRatio(){
     let rect=elem.getBoundingClientRect();
     if( rect.height==0 || rect.width==0 ){
         var s=( (tue_size[0]/window.innerWidth)>(tue_size[1]/window.innerHeight) ) ? window.innerWidth / tue_size[0] : window.innerHeight / tue_size[1];
-        tuesday.style.left=(window.innerWidth-(tue_size[0]*s))/2+"px";
-        tuesday.style.top=(window.innerHeight-(tue_size[1]*s))/2+"px";
+        tuesday.style.left=(window.innerWidth-(tue_size[0]*s))/2+'px';
+        tuesday.style.top=(window.innerHeight-(tue_size[1]*s))/2+'px';
     }else{
         var s=( (tue_size[0]/rect.width)>(tue_size[1]/rect.height) ) ? rect.width / tue_size[0] : rect.height / tue_size[1];
-        tuesday.style.left=(rect.width-(tue_size[0]*s))/2+"px";
-        tuesday.style.top=(rect.height-(tue_size[1]*s))/2+"px";
+        tuesday.style.left=(rect.width-(tue_size[0]*s))/2+'px';
+        tuesday.style.top=(rect.height-(tue_size[1]*s))/2+'px';
     }
-    elem.style.backgroundColor="#000";
-    elem.style.overflow="hidden";
-    elem.style.position="relative";
-    tuesday.style.position="absolute";
-    tuesday.style.transformOrigin="left top";
-    tuesday.style.width =tue_size[0]+"px";
-    tuesday.style.height=tue_size[1]+"px";
+    elem.style.backgroundColor='#000';
+    elem.style.overflow='hidden';
+    elem.style.position='relative';
+    tuesday.style.position='absolute';
+    tuesday.style.transformOrigin='left top';
+    tuesday.style.width =tue_size[0]+'px';
+    tuesday.style.height=tue_size[1]+'px';
     tuesday.style.transform='scale('+s+')';
 }
 window.addEventListener('script_executed',TueAspectRatio,true);
