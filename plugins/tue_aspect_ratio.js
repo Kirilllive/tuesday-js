93b4d81a2af61d251123b2e2e2a6bc051d3a744d
Kirill Live <kirill_@live.ru>
2025-03-26 00:01:24 +0300

    kirilllive

--- plugins/tue_aspect_ratio.js
+++ plugins/tue_aspect_ratio.js
@@ -3,11 +3,11 @@ function TueAspectRatio(){
     let elem=tuesday.parentNode;
     let rect=elem.getBoundingClientRect();
     if( rect.height==0 || rect.width==0 ){
-        var s=( (tue_size[0]/window.innerWidth)>(tue_size[1]/window.innerHeight) ) ? window.innerWidth / tue_size[0] : window.innerHeight / tue_size[1];
+        var s=(story_json.parameters.resolutions[4])?((tue_size[0]/window.innerWidth)>(tue_size[1]/window.innerHeight))?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1]:((tue_size[0]/window.innerWidth)<(tue_size[1]/window.innerHeight))?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1];
         tuesday.style.left=(window.innerWidth-(tue_size[0]*s))/2+'px';
         tuesday.style.top=(window.innerHeight-(tue_size[1]*s))/2+'px';
     }else{
-        var s=( (tue_size[0]/rect.width)>(tue_size[1]/rect.height) ) ? rect.width / tue_size[0] : rect.height / tue_size[1];
+        var s=(story_json.parameters.resolutions[4])?((tue_size[0]/rect.width)<(tue_size[1]/rect.height))?rect.width/tue_size[0]:rect.height/tue_size[1]:((tue_size[0]/rect.width)>(tue_size[1]/rect.height))?rect.width/tue_size[0]:rect.height/tue_size[1];
         tuesday.style.left=(rect.width-(tue_size[0]*s))/2+'px';
         tuesday.style.top=(rect.height-(tue_size[1]*s))/2+'px';
     }
@@ -25,3 +25,5 @@ function TueAspectRatio(){
 }
 window.addEventListener('script_executed',TueAspectRatio,true);
 window.addEventListener('resize',TueAspectRatio,true);
+
+
