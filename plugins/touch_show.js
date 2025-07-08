9d1489efa85af05fe3388e6e6a223cda85fd1bcf
Odin-interactive <kirill_@live.ru>
2025-07-08 22:44:38 +0300

    kirilllive

--- plugins/touch_show.js
+++ plugins/touch_show.js
@@ -1,17 +1,16 @@
-
 tuesday.addEventListener('script_loaded',function(){
     var tuesday_cursor_style = document.createElement('style');
     tuesday_cursor_style.type='text/css';
-    tuesday_cursor_style.innerHTML=".cursor{"+story_json.parameters.cursors.click[3]+";transform:translate(-50%,-50%);position:absolute;display:block;animation-name:cursor_anim;animation-fill-mode:forwards;}@keyframes cursor_anim{from{opacity:1;}to{opacity:0;}}";
+    tuesday_cursor_style.innerHTML=".cursor{background: center / contain no-repeat url('"+story_json.parameters.touch_show[0]+"');"+story_json.parameters.touch_show[4]+";transform:translate(-50%,-50%);position:absolute;display:block;animation-name:cursor_anim;animation-fill-mode:forwards;}@keyframes cursor_anim{from{opacity:1;}to{opacity:0;}}";
     document.getElementsByTagName('head')[0].appendChild(tuesday_cursor_style);
     tuesday.addEventListener('mousedown',(e)=>{
-        const click=story_json.parameters.cursors.click;
+        const click=story_json.parameters.touch_show;
         let tuesday_cursor=document.createElement("div");
         let rect = tuesday.getBoundingClientRect();
-        tuesday_cursor.className="cursor";
-        tuesday_cursor.style.width=click[0]+"px";
-        tuesday_cursor.style.height=click[1]+"px";
-        tuesday_cursor.style.animationDuration=click[2]+"s";
+        tuesday_cursor.className="cursor "+story_json.parameters.touch_show[5];
+        tuesday_cursor.style.width=click[1];
+        tuesday_cursor.style.height=click[2];
+        tuesday_cursor.style.animationDuration=click[3]+"s";
         if(story_json.parameters.resolutions){
             const r=story_json.parameters.resolutions
             tuesday_cursor.style.left=(((r[0]/rect.width)>(r[1]/rect.height)?(r[0]/rect.width):(r[1]/rect.height))*(e.clientX-rect.left))+"px";
@@ -21,9 +20,7 @@ tuesday.addEventListener('script_loaded',function(){
             tuesday_cursor.style.top=(e.clientY-rect.top)+"px";
         }
         
-        setTimeout(function(e){tuesday_cursor.remove();},(click[2]*1000));
+        setTimeout(function(e){tuesday_cursor.remove();},(click[3]*1000));
         tuesday.appendChild(tuesday_cursor);
     })
 })
-
-
