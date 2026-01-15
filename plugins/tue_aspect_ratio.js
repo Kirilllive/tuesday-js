429fd42ade3aa3744b1d01cb760f692ef5e0fdb3
Odin-interactive <kirill_@live.ru>
2026-01-16 00:51:56 +0300

    kirillllive

--- plugins/tue_aspect_ratio.js
+++ plugins/tue_aspect_ratio.js
@@ -3,14 +3,20 @@ function TueAspectRatio(){
     let elem=tuesday.parentNode;
     let rect=elem.getBoundingClientRect();
     let scale;
-    if( rect.height==0 || rect.width==0 ){
-        scale=(story_json.parameters.resolutions[4])?((tue_size[0]/window.innerWidth)<(tue_size[1]/window.innerHeight))?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1]:((tue_size[0]/window.innerWidth)>(tue_size[1]/window.innerHeight))?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1];
-        tuesday.style.left=(window.innerWidth-(tue_size[0]*scale))/2+'px';
-        tuesday.style.top=(window.innerHeight-(tue_size[1]*scale))/2+'px';
-    }else{
-        scale=(story_json.parameters.resolutions[4])?((tue_size[0]/rect.width)<(tue_size[1]/rect.height))?rect.width/tue_size[0]:rect.height/tue_size[1]:((tue_size[0]/rect.width)>(tue_size[1]/rect.height))?rect.width/tue_size[0]:rect.height/tue_size[1];
-        tuesday.style.left=(rect.width-(tue_size[0]*scale))/2+'px';
-        tuesday.style.top=(rect.height-(tue_size[1]*scale))/2+'px';
+    if(tue_size[4]){
+        scale=(window.innerWidth<window.innerHeight)?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1];
+        elem.style.margin="0";
+        elem.style.overscrollBehavior="none";
+    } else {
+        if(rect.height==0||rect.width==0){
+            scale=((tue_size[0]/window.innerWidth)>(tue_size[1]/window.innerHeight))?window.innerWidth/tue_size[0]:window.innerHeight/tue_size[1];
+            tuesday.style.left=(window.innerWidth-(tue_size[0]*scale))/2+'px';
+            tuesday.style.top=(window.innerHeight-(tue_size[1]*scale))/2+'px';
+        }else{
+            scale=((tue_size[0]/rect.width)>(tue_size[1]/rect.height))?rect.width/tue_size[0]:rect.height/tue_size[1];
+            tuesday.style.left=(rect.width-(tue_size[0]*scale))/2+'px';
+            tuesday.style.top=(rect.height-(tue_size[1]*scale))/2+'px';
+        }
     }
     elem.style.backgroundColor=(tue_size[2]&&tue_size[2].length>0)?tue_size[2]:'#000';
     elem.style.backgroundImage="url('"+tue_size[3]+"')";
@@ -20,8 +26,8 @@ function TueAspectRatio(){
     elem.style.position='relative';
     tuesday.style.position='absolute';
     tuesday.style.transformOrigin='left top';
-    tuesday.style.width =tue_size[0]+'px';
-    tuesday.style.height=tue_size[1]+'px';
+    tuesday.style.width =(tue_size[4]?window.innerWidth/scale:tue_size[0])+'px';
+    tuesday.style.height=(tue_size[4]?window.innerHeight/scale:tue_size[1])+'px';
     tuesday.style.transform='scale('+scale+')';
 }
 window.addEventListener('script_executed',TueAspectRatio,true);
